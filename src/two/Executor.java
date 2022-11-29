6485c9da265421a51ea0a66e36f22ebcf40119a0
Azemina Beganovic Rizvanovic <117936928+azeminab@users.noreply.github.com>
2022-11-29 20:11:53 +0100

    Executor s main funkcijom kreiran

--- src/two/Executor.java
+++ src/two/Executor.java
@@ -2,6 +2,25 @@ package two;
 
 public class Executor {
     public static void main(String[] args) {
+        // byte - 8 bita
+        byte prviBroj = -128;
+        // short - 16 bita
+        short drugiBroj = -32768;
+        // int - 32 bita
+        int treciBroj = -32769578;
+        // long - 64 bita
+        long cetvrtiBroj = 4569785603457l;
+        //float 32 bita
+        float prviDecimalniBroj = 23.4f;
+        // double 64 bita
+        double drugiDecimalniBroj = 34.2;
+        // char
+        char slovo = 65;
+        System.out.println(slovo);
+        //boolean
+        boolean condition = false;
+
+
 
     }
 }
---
+++ src/two/QuestionExecutor.java
@@ -0,0 +1,16 @@
+package two;
+
+public class QuestionExecutor {
+
+    public static void main(String[] args) {
+        //64 decimal number
+        double doubleNumber = 23.0;
+        //32 int number
+        int number = (int)doubleNumber;
+
+        //32 decimal number
+        float floatingNumber = 23.0f;
+        //64 decmial number
+        double doubleNumber2 = floatingNumber;
+    }
+}
